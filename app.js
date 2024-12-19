0008e7bf198422e0ed039d7ea56ad2f6a69a7331
Muhammad Osama <upatni939@gmail.com>
2024-12-19 16:38:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+// Chapter Number 1
+//  Question 1// Write a script to display following message on your web page:
+alert('Error!Please enter a valid password')
+// Question 2Write a script to display following message on your web
+// page: (Hint : Use line break)
+alert('Welcome to jS land../nhappy coding')
+// Question 3 Write a script to display following messages in sequence:
+alert('Welcome to js land..')
+// Q 4. Write a script to display following messages in sequence:
+
+alert("Welcome to js land");
+ confirm("prevent this page from Creating additional dialogs");
+ 
+//  Q 5 Generate the following message through browser’s developer console:
+alert('Hello... I can run js through my web browser console')
+
+
+
+
+// Chapter Number 2 VARIABLES FOR STRINGS
+// Question 1 Declare a variable called username.
+var myName= ("Muhammad Osama");
+alert(myName);
+//var message= ("Hello World");
+//alert(message);
+
+var bioData= ("Certified Mobile Application Development");
+alert(bioData);
+
+//Question No 2
+
+var foodName=("Pizza\nPizz\nPiz\nPi\nP");
+alert(foodName);
+
+//Question No 3
+
+var email=("u2391460@gmail.com");
+alert(email);
+
+//Question No 4
+
+var book=("A smarter way to learn Java Script");
+alert(book);
+
+//Question No 5
+
+//document.write("<h1>Thank you for choosing us as your next career move.We're very proud to be welcoming you to our company.</h1>")
+
+//Chapter No 3 Variables for Numbers
+
+//Question No 1
+
+var age=(18);
+alert(age);
+
+//Question No 2
+
+var visitors=("You have visited this site 14 times.");
+alert(visitors);
+
+//Question No 3
+
+var birthYear="2006"
+document.write("<h4>" + birthYear + "</h4>");
+
+//Question No 4
+
+
+
